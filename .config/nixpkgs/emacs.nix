21138706eebee515145ad292198643f830b52f38
Lukas Haigner <code@firy.io>
2022-08-20 02:25:07 +0200

    Try to fix Vue 3 using emacsPackagesOverlay

--- .config/nixpkgs/emacs.nix
+++ .config/nixpkgs/emacs.nix
@@ -6,6 +6,17 @@ let
   }) {
     doomPrivateDir = ./doom.d;  # Directory containing your config.el init.el
                                 # and packages.el files
+    emacsPackagesOverlay = prev: final: {
+      pname = "lsp-volar";
+      version = "git";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "jadestrong";
+        repo = "lsp-volar";
+        rev = "0ccdf06c409120f0d060e9a43cdf3afe05f57ed1";
+        sha256 = "0j9gy5wqlcqrpynyqjgknc5kldpy24rj0yb0d29p9wqvbr993p8g";
+      };
+    };
   };
 in {
   home.packages = [ doom-emacs ];
