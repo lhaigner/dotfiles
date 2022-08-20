2408faf55809c0e48f4e7ec330378b4ba1b446ca
Lukas Haigner <code@firy.io>
2022-08-20 03:21:01 +0200

    Fix emacsPackagesOverlay syntax

--- .config/nixpkgs/emacs.nix
+++ .config/nixpkgs/emacs.nix
@@ -7,15 +7,17 @@ let
     doomPrivateDir = ./doom.d;  # Directory containing your config.el init.el
                                 # and packages.el files
     emacsPackagesOverlay = prev: final: {
-      pname = "lsp-volar";
-      version = "git";
+      lsp-volar = (prev.trivialBuild {
+        pname = "lsp-volar";
+        version = "git";
 
-      src = pkgs.fetchFromGitHub {
-        owner = "jadestrong";
-        repo = "lsp-volar";
-        rev = "0ccdf06c409120f0d060e9a43cdf3afe05f57ed1";
-        sha256 = "0j9gy5wqlcqrpynyqjgknc5kldpy24rj0yb0d29p9wqvbr993p8g";
-      };
+        src = pkgs.fetchFromGitHub {
+          owner = "jadestrong";
+          repo = "lsp-volar";
+          rev = "0ccdf06c409120f0d060e9a43cdf3afe05f57ed1";
+          sha256 = "0j9gy5wqlcqrpynyqjgknc5kldpy24rj0yb0d29p9wqvbr993p8g";
+        };
+      });
     };
   };
 in {
